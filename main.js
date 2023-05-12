2931ebeb6e61d72b101b60233a0eb6065164d35b
3thernity <3thernitydev@gmail.com>
2023-05-12 14:59:45 +0400

    a lot of change

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
+let toggle = document.querySelector(".toggle");
+let body = document.querySelector("body");
 
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
-
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    sidenav.classList.add("active");
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    sidenav.classList.remove("active");
-}
+toggle.addEventListener("click", function () {
+    body.classList.toggle("open");
+});
